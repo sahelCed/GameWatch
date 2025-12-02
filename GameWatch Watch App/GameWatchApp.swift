167c3928c370d76f780c466d85ae9e6a5e79735a
yanislammari <yanis.lammari937@gmail.com>
2025-12-02 18:40:58 +0100

    Initial commit

---
+++ GameWatch Watch App/GameWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameWatchApp.swift
+//  GameWatch Watch App
+//
+//  Created by Yanis Lammari on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct GameWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GameWatch/GameWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameWatchApp.swift
+//  GameWatch
+//
+//  Created by Yanis Lammari on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct GameWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
